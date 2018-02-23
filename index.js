0d282b106a28e1e8de679073813c700c2d59bc5a
Ian Duke <ian@thefarmventures.com>
2018-02-23 15:19:30 -0500

    placeholder promise:

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.hears('wow', (ctx) => {
   ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message.id)
+  .then(setTimeout(function() { console.log('hello bird') }, 100))
 });
 
 module.exports = bot;

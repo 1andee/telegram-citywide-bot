2c8ca5ed3a49a8f4794fa031461ebf6bdf78ba8e
Ian Duke <ian@thefarmventures.com>
2018-02-23 15:26:34 -0500

    replace placeholder with catch

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const { reply } = Telegraf
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.hears('wow', (ctx) => {
+  console.log(ctx.message);
   ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message.id)
-  .then(setTimeout(function() { console.log('hello bird') }, 100))
+  .catch((err) => {
+    console.log('Ooops', err);
+  });
 });
 
 module.exports = bot;

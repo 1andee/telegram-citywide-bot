09ba6839e710bba7f70d7ee9e8eef6b43a7c0567
Ian Duke <ian@thefarmventures.com>
2018-02-23 15:11:15 -0500

    test deleteMessage

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const Telegraf = require('telegraf');
 const { reply } = Telegraf
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.hears('wow', (ctx) => ctx.reply('wow detected'));
-bot.hears('Wow', (ctx) => ctx.reply('Sentence case Wow detected'));
+bot.hears('wow', (ctx) => {
+  ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message.id)
+});
 
 module.exports = bot;

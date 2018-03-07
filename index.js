590e843e6b488f6a6344443d509ae3951fe0b36b
Ian Duke <ian.c.duke@gmail.com>
2018-03-06 19:48:45 -0500

    add log first, stop catch block from crashing app

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const { reply } = Telegraf
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.hears(/\bwow\b/i, (ctx) => {
-  ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id);
   console.log(`############\nDELETING MESSAGE:\n${ctx.message}`);
+  ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id)
   .catch((err) => {
     console.log('Something blew up:', err);
   });
 });
 
 bot.hears(/^acknowledged$/i, (ctx) => {
-  ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id);
   console.log(`############\nDELETING MESSAGE:\n${ctx.message}`);
+  ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id)
   .catch((err) => {
     console.log('Something blew up:', err);
   });

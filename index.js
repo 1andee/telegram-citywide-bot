be613d31e12be17f303e7cd6a293017b842568fa
Ian Duke <ian.c.duke@gmail.com>
2018-03-06 19:46:28 -0500

    log messages flowing through app

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const { reply } = Telegraf
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.hears(/\bwow\b/i, (ctx) => {
-  ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id)
+  ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id);
+  console.log(`############\nDELETING MESSAGE:\n${ctx.message}`);
   .catch((err) => {
     console.log('Something blew up:', err);
   });
 });
 
 bot.hears(/^acknowledged$/i, (ctx) => {
-  ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id)
+  ctx.telegram.deleteMessage(ctx.message.chat.id, ctx.message.message_id);
+  console.log(`############\nDELETING MESSAGE:\n${ctx.message}`);
   .catch((err) => {
     console.log('Something blew up:', err);
   });

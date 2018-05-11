d4cd301166b4be4d75d66cb89dbc8251913ee7fd
Ian Duke <ian.c.duke@gmail.com>
2018-05-11 19:11:47 -0400

    debug command

--- index.js
+++ index.js
@@ -21,8 +21,10 @@ bot.hears(/^acknowledge/i, (ctx) => {
 });
 
 bot.command('/reminder', (ctx) => {
+  console.log('inside reminder');
   if (admin(ctx.message.from.username)) {
-    ctx.reply("A reminder... CH-1 is only for confirmed incidents or significant updates. Please use CH-2 for any side chatter, pics, info about rehab units, etc. Many of us have notifications only active on CH-1, and we don’t all have normal schedules, so even a message in the middle of the day may be waking someone up. Any questions, just let John, Ian or Jeremy know.")
+    console.log('admin recognized');
+    ctx.telegram.reply("A reminder... CH-1 is only for confirmed incidents or significant updates. Please use CH-2 for any side chatter, pics, info about rehab units, etc. Many of us have notifications only active on CH-1, and we don’t all have normal schedules, so even a message in the middle of the day may be waking someone up. Any questions, just let John, Ian or Jeremy know.")
     .catch((err) => {
       console.log('Something blew up:', err);
     });

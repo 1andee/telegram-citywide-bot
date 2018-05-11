ddf3d34d2a0707b7cbe89a451657d29764ecbae2
Ian Duke <ian.c.duke@gmail.com>
2018-05-11 19:16:45 -0400

    revert ctx reply

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ bot.command('/reminder', (ctx) => {
   console.log('inside reminder');
   if (admin(ctx.message.from.username)) {
     console.log('admin recognized');
-    ctx.telegram.reply("A reminder... CH-1 is only for confirmed incidents or significant updates. Please use CH-2 for any side chatter, pics, info about rehab units, etc. Many of us have notifications only active on CH-1, and we don’t all have normal schedules, so even a message in the middle of the day may be waking someone up. Any questions, just let John, Ian or Jeremy know.")
+    ctx.reply("A reminder... CH-1 is only for confirmed incidents or significant updates. Please use CH-2 for any side chatter, pics, info about rehab units, etc. Many of us have notifications only active on CH-1, and we don’t all have normal schedules, so even a message in the middle of the day may be waking someone up. Any questions, just let John, Ian or Jeremy know.")
     .catch((err) => {
       console.log('Something blew up:', err);
     });
